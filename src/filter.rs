e4d2fee9b9ff715ab7c87dcfc3812159b139112e
yunshuiz-unimelb <yunshuiz@student.unimelb.edu.au>
2022-01-04 16:56:53 +1100

    add test for run tf graph

--- src/filter.rs
+++ src/filter.rs
@@ -1,6 +1,6 @@
 use libafl::inputs::HasBytesVec;
 use libafl::observers::{map::MapObserver, ObserversTuple};
-use num_traits::PrimInt;
+use num_traits::{cast::cast, PrimInt};
 use std::fmt::Debug;
 use std::marker::PhantomData;
 use tensorflow as tf;
@@ -43,10 +43,13 @@ where
     name: String,
     batch_size: usize,
 
-    num_obv: usize,
-    num_in: usize,
+    num_obv: usize, // number of real execution
+    num_in: usize,  // number of inputs fed in
 
-    // model: utils::Model,
+    obv_xs: Vec<Vec<u8>>,
+    obv_ys: Vec<Vec<f32>>,
+
+    model: utils::Model,
     phantom: PhantomData<(I, O, T)>,
 }
 
@@ -56,12 +59,19 @@ where
     T: tf::TensorType + PrimInt + Clone + Debug,
 {
     pub fn new(name: &str, batch_size: usize) -> Self {
+        let model = utils::Model::new(name);
+        let obv_xs = Vec::with_capacity(batch_size);
+        let obv_ys = Vec::with_capacity(batch_size);
         Self {
             name: name.to_string(),
             batch_size,
+
             num_in: 0,
             num_obv: 0,
-            // model: utils::Model::new(name: &str, in_dim: usize, out_dim: usize),
+            obv_xs,
+            obv_ys,
+
+            model,
             phantom: PhantomData,
         }
     }
@@ -78,36 +88,48 @@ where
     }
 
     fn run(&mut self, batch: Vec<I>, _state: &mut S, _corpus_id: usize) -> (Vec<I>, Vec<f32>) {
-        // TODO: (yun) just for test make all inputs pass the filter
         self.num_in += batch.len();
-
-        // create the tensor for inputs
         let xs: Vec<&[u8]> = batch.iter().map(|x| x.bytes()).collect();
-        // TODO:
-        // let xs = tf::Tensor::
-
-        let prob = batch.iter().map(|_| 1.0).collect();
+        let ys = self.model.predict(&xs);
+        let prob = self.filter(ys);
         (batch, prob)
     }
 
-    fn observe<OT: ObserversTuple<I, S>>(&mut self, observers: &OT, _input: &I) {
+    fn observe<OT: ObserversTuple<I, S>>(&mut self, observers: &OT, input: &I) {
         let observer = observers.match_name::<O>(&self.name).unwrap();
-        let _output = observer.as_tensor().unwrap();
+        let map = observer.map().unwrap();
         self.num_obv += 1;
-        if self.num_obv % 1000 == 0 {
-            println!("Filter observed {}", self.num_obv);
+        // observe the sample
+        if self.obv_xs.len() == self.batch_size {
+            let xs: Vec<_> = self.obv_xs.iter().map(|x| x.as_slice()).collect();
+            let ys: Vec<_> = self.obv_ys.iter().map(|y| y.as_slice()).collect();
+            self.model.train(&xs, &ys, 32); // TODO: hardcode, train for 32 steps
+            self.obv_xs.truncate(0);
+            self.obv_ys.truncate(0);
         }
-        // TODO: (yun) observe the sample
+        self.obv_xs.push(input.bytes().to_vec());
+        self.obv_ys
+            .push(map.iter().map(|&t| cast(t).unwrap()).collect());
+    }
+}
+
+impl<I, O, T> CovFilter<I, O, T>
+where
+    I: HasBytesVec,
+    O: MapObserver<T>,
+    T: tf::TensorType + PrimInt + Clone + Debug,
+{
+    fn filter(&self, _ys: tf::Tensor<f32>) -> Vec<f32> {
+        todo!("yun")
     }
 }
 
 mod utils {
     use super::tf;
-    use std::mem::size_of;
+    use super::tf::Tensor;
     use std::path;
     use std::process::Command;
     use std::ptr;
-    use tensorflow::{SignatureDef, Tensor};
 
     pub const PY: &str = "python3";
     pub const SCRIPT: &str = "src/py/create_model.py";
@@ -144,21 +166,26 @@ mod utils {
         (graph, bundle)
     }
 
+    #[derive(Debug)]
     pub struct Model {
-        name: String,
-        in_dim: usize,
-        out_dim: usize,
+        pub in_dim: usize,
+        pub out_dim: usize,
         graph: tf::Graph,
         bundle: tf::SavedModelBundle,
     }
 
     impl Model {
-        pub fn new(name: &str, in_dim: usize, out_dim: usize) -> Self {
+        pub fn new(name: &str) -> Self {
             let (graph, bundle) = load_tf_model(name);
+            let sig = bundle.meta_graph_def().get_signature("train").unwrap();
+            let x_shape = sig.get_input("x").unwrap().shape();
+            let y_shape = sig.get_input("y").unwrap().shape();
+            println!("xshape: {}", x_shape);
+            println!("yshape: {}", y_shape);
+
             Self {
-                name: name.to_string(),
-                in_dim,
-                out_dim,
+                in_dim: x_shape[1].unwrap() as usize,
+                out_dim: y_shape[1].unwrap() as usize,
                 graph,
                 bundle,
             }
@@ -185,22 +212,21 @@ mod utils {
                 .unwrap()
                 .unwrap();
 
-            let mut run_args = tf::SessionRunArgs::new();
-            run_args.add_feed(&op_x, 0, &xs);
-            run_args.add_target(&op_y_hat);
-            session.run(&mut run_args).unwrap();
+            let mut step = tf::SessionRunArgs::new();
+            step.add_feed(&op_x, 0, &xs);
+            step.add_target(&op_y_hat);
+            let y_hat_tok = step.request_fetch(&op_y_hat, 0);
+            session.run(&mut step).unwrap();
 
-            let mut output_step = tf::SessionRunArgs::new();
-            let y_hat_ix = output_step.request_fetch(&op_y_hat, 0);
-            session.run(&mut output_step).unwrap();
+            // let mut output_step = tf::SessionRunArgs::new();
+            // let y_hat_ix = output_step.request_fetch(&op_y_hat, 0);
+            // session.run(&mut output_step).unwrap();
 
-            let y_hat: Tensor<f32> = output_step.fetch(y_hat_ix).unwrap();
+            let y_hat: Tensor<f32> = step.fetch(y_hat_tok).unwrap();
             y_hat
         }
 
-        pub fn train(&self, xs: &[&[u8]], ys: &[&[u8]]) {
-            const N_STEP: usize = 100; // number of train steps
-
+        pub fn train(&self, xs: &[&[u8]], ys: &[&[f32]], nstep: usize) {
             let xs = Self::new_tensor_from(xs, self.in_dim);
             let ys = Self::new_tensor_from(ys, self.out_dim);
 
@@ -225,15 +251,13 @@ mod utils {
             step.add_feed(&op_x, 0, &xs);
             step.add_feed(&op_y, 0, &ys);
             step.add_target(&op_loss);
+
             let session = &self.bundle.session;
-            for _ in 0..N_STEP {
+            for _ in 0..nstep {
                 session.run(&mut step).unwrap();
             }
         }
-    }
 
-    // private methods
-    impl Model {
         // create a new tensor and copy the inputs to the tensor
         fn new_tensor_from<T: tf::TensorType>(xs: &[&[T]], dim: usize) -> tf::Tensor<T> {
             let n = xs.len(); // number of samples/inputs
@@ -242,7 +266,7 @@ mod utils {
             // copy xs to tensor
             unsafe {
                 for x in xs {
-                    ptr::copy_nonoverlapping(x.as_ptr(), ts_ptr, size_of::<T>() * x.len());
+                    ptr::copy_nonoverlapping(x.as_ptr(), ts_ptr, x.len());
                     ts_ptr = ts_ptr.add(dim);
                 }
             }
@@ -254,6 +278,7 @@ mod utils {
 #[cfg(test)]
 mod tests {
 
+    use super::tf;
     use super::utils::*;
     use std::process::Command;
 
@@ -277,13 +302,18 @@ mod tests {
 
     #[test]
     fn test_tf_model() {
-        const IN_DIM: usize = 512;
-        const OUT_DIM: usize = 2048;
-
-        let model = Model::new("test-model", IN_DIM, OUT_DIM);
+        // test model load
+        let model = Model::new("test-model");
+        assert_eq!(512, model.in_dim);
+        assert_eq!(2048, model.out_dim);
+        // test predict
         let x: Vec<u8> = vec![0 as u8, 0 as u8, 0 as u8];
         let y_hat = model.predict(&[&x]);
+        assert_eq!(
+            y_hat.shape(),
+            tf::Shape::new(Some(vec![Some(1), Some(model.out_dim as i64)]))
+        );
 
-        println!("y_hat: {}", y_hat);
+        // test train
     }
 }
